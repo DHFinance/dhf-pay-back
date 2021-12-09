bac5bab4380c2058635fa66b7dccb9cffbb883de
JX-29 <anton23490@gmail.com>
2021-12-09 15:27:25 +0300

    stores entity

---
+++ src/stores/stores.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, HttpException, HttpStatus, Inject, Post } from "@nestjs/common";
+import {
+  Crud,
+  CrudController,
+  Override,
+  CrudRequest,
+  ParsedRequest,
+  ParsedBody,
+  CreateManyDto,
+} from '@nestjsx/crud';
+import { StoresService } from "./stores.service";
+import { ClientProxy } from "@nestjs/microservices";
+import { SCondition } from "@nestjsx/crud-request";
+import { Stores } from "./entities/stores.entity";
+
+@Crud({
+  model: {
+    type: Stores,
+  },
+  query: {
+    join: {
+      user: {
+        eager: true,
+      },
+      transaction: {
+        eager: true,
+      },
+    },
+  },
+})
+
+@Controller('payment')
+export class StoresController implements CrudController<Stores> {
+  constructor(
+    public readonly service: StoresService,
+
+  ) {}
+
+}
+
