ab38b186d743ceda64ebf7ce87193b308c9c3077
JX-29 <anton23490@gmail.com>
2021-12-22 14:04:46 +0300

    delete admin

--- src/migrations/1631542698364-add-admin.ts
+++ src/migrations/1631542698364-add-admin.ts
@@ -6,11 +6,11 @@ export class addAdmin1631542698364 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `INSERT INTO "user" ("name", "lastName", "password", "email", "role", "company", "token", "blocked") VALUES ('admin', 'admin', '95f8de6d395f4844d070053d6e7fe223b949e2e8', '${env.ADMIN_EMAIL}', 'admin', 'smartigy', '5ZlEqFyVD4XMnxJsSFZf2Yra1k3m44o1E59v', false)`,
+      `INSERT INTO "user" ("name", "lastName", "password", "email", "role", "company", "token", "blocked") VALUES ('admin', 'admin', 'd7ce7ea83f5f72928a80c116473513efca088d7c', '${env.ADMIN_EMAIL}', 'admin', 'smartigy', '5ZlEqFyVD4XMnxJsSFZf2Yra1k3m44o1E59v', false)`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DELETE FROM user`);
+    await queryRunner.query(`DELETE FROM "user"`);
   }
 }
