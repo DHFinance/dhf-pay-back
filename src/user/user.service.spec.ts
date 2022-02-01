fd8ac76e1ec512baf4f31745f46333cb9dd719c6
Yousheng21 <ermachenkovvova@gmail.com>
2022-02-01 17:14:03 +0300

    test user.service

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -8,9 +8,21 @@ import * as path from "path";
 
 const dotEnvPath = path.resolve(__dirname, '..', '.env');
 
-describe('UserService', () => {
+const user = {
+  name:"1",
+  lastName:"1",
+  email:"mail@gmail.com",
+  // token:"$2b$07$PUx7RK/NjXwo7i9xpYT2vejPjU3A4hxCCvYYkDbZ/fcfgyFnCw9f.",
+  role:"customer",
+  // id: 60,
+  password:"5ZlEqFyVD4XMnxJsSFZf2Yra1k3m44o1E59v",
+  company:"mail.ru"
+};
+
+describe('UserService',() => {
   let service: UserService;
   let mailerService: MailerService;
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -31,15 +43,14 @@ describe('UserService', () => {
           inject: [ConfigService],
         }),
         TypeOrmModule.forFeature([User]),
-        MailerModule
+        MailerModule,
       ],
       providers: [
         UserService,
         {
           provide: MailerService,
           useValue: {
-            get: jest.fn(() => {
-
+            get: jest.fn(async () => {
             }),
             // really it can be anything, but the closer to your actual logic the better
           }
@@ -49,6 +60,9 @@ describe('UserService', () => {
 
     service = module.get<UserService>(UserService);
     mailerService = module.get<MailerService>(MailerService);
+
+    mailerService.sendMail = jest.fn();
+
   });
 
   it('should be defined', () => {
@@ -56,30 +70,34 @@ describe('UserService', () => {
     expect(service).toBeDefined();
   });
 
+  it('should created user',  async () => {
+    const createdUser = await service.create({ ...user, blocked: false });
+    expect(createdUser).toEqual(true);
+
+  });
+
+  it('should get error at creating user',  async () => {
+    await expect(service.create(user)).rejects.toThrow();
+  });
+
+
+  it('should get error at verifying user',  async () => {
+    await expect(service.verifyUser(user.email, 5454)).rejects.toThrow();
+  });
+
+  it('should changed block',  async () => {
+    let foundedUser = await service.findByEmail(user.email);
+    const change = await service.changeBlockUser(foundedUser.id,true);
+    expect(change).toHaveProperty("blocked",true);
+  });
+
   it('find user by email',async () => {
-    const user = {
-      name:"1",
-      lastName:"1",
-      email:"mail@gmail.com",
-      token:"$2b$07$PUx7RK/NjXwo7i9xpYT2vejPjU3A4hxCCvYYkDbZ/fcfgyFnCw9f.",
-      role:"customer",
-      id: 60,
-      password:"5ZlEqFyVD4XMnxJsSFZf2Yra1k3m44o1E59v",
-      company:"mail.ru"
-    };
-
-    // @ts-ignore
-    await User.save({ ...user, blocked: false });
-
-    const foundedUser = await User.findOne({
-      where: {
-        email: user.email
-      },
-    })
+    const foundedUser = await service.findByEmail(user.email);
 
     expect(foundedUser).toHaveProperty("email","mail@gmail.com");
 
     // @ts-ignore
-    await User.remove({...foundedUser})
+    // await User.remove({...foundedUser});
   });
+
 });
