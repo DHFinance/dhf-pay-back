4c37b7388d473d383660ba51efb89435e8fdc56f
Enflow.io <konstantin@enflow.io>
2022-04-02 13:39:30 +0300

    stores security

--- src/stores/stores.controller.ts
+++ src/stores/stores.controller.ts
@@ -1,7 +1,8 @@
-import {Body, Controller, Patch, Post, Param, Headers, Get} from "@nestjs/common";
+import {Body, Controller, Patch, Post, Param, Headers, Get, UnauthorizedException} from "@nestjs/common";
 import {
+  CreateManyDto,
   Crud,
-  CrudController, CrudRequest, Override, ParsedRequest,
+  CrudController, CrudRequest, Override, ParsedBody, ParsedRequest,
 } from '@nestjsx/crud';
 import { StoresService } from "./stores.service";
 import { Stores } from "./entities/stores.entity";
@@ -34,6 +35,63 @@ export class StoresController implements CrudController<Stores> {
     public readonly service: StoresService,
   ) {}
 
+  get base(): CrudController<Stores> {
+    return this;
+  }
+
+
+  @Override()
+  getMany(
+      @ParsedRequest() req: CrudRequest,
+  ) {
+    throw new UnauthorizedException()
+  }
+
+
+
+  @Override()
+  createOne(
+      @ParsedRequest() req: CrudRequest,
+      @ParsedBody() dto: Stores,
+      @Headers() token
+  ) {
+    if(!token?.authorization){
+      throw new UnauthorizedException()
+    }
+    return this.base.createOneBase(req, dto);
+  }
+
+  @Override()
+  createMany(
+      @ParsedRequest() req: CrudRequest,
+      @ParsedBody() dto: CreateManyDto<Stores>
+  ) {
+    throw new UnauthorizedException()
+  }
+
+  @Override('updateOneBase')
+  coolFunction(
+      @ParsedRequest() req: CrudRequest,
+      @ParsedBody() dto: Stores,
+  ) {
+    return this.base.createOneBase(req, dto);
+  }
+
+  @Override('replaceOneBase')
+  awesomePUT(
+      @ParsedRequest() req: CrudRequest,
+      @ParsedBody() dto: Stores,
+  ) {
+    throw new UnauthorizedException()
+  }
+
+  @Override()
+  async deleteOne(
+      @ParsedRequest() req: CrudRequest,
+  ) {
+    throw new UnauthorizedException()
+  }
+
 
   @Post('block')
   @ApiProperty({ type: BlockStoreDto })
@@ -45,6 +103,9 @@ export class StoresController implements CrudController<Stores> {
   @Get('tx/:token')
   @ApiProperty()
   async getAllStores(@Headers() token) {
+    if(!token?.authorization){
+      throw new UnauthorizedException()
+    }
     return this.service.getAllStore(token.authorization.split(' ')[1])
   }
 
@@ -52,6 +113,9 @@ export class StoresController implements CrudController<Stores> {
   @Get(':id')
   @ApiProperty()
   async getUserStores(@Param() id, @Headers() token) {
+    if(!token?.authorization){
+      throw new UnauthorizedException()
+    }
     return this.service.getUserStores(id, token.authorization.split(' ')[1]);
   }
 
@@ -59,6 +123,9 @@ export class StoresController implements CrudController<Stores> {
   @Patch(':id')
   @ApiProperty()
   async updateStore(@Body() body, @Param() id, @Headers() header) {
+    if(!header.authorization){
+      throw new UnauthorizedException()
+    }
     return this.service.updateStore(body, id, header.authorization.split(' ')[1]);
   }
 }
