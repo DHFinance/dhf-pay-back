1b18176714863b05fd3c962b74a25c5324ff520b
Andrey Krylov <reppin040599@gmail.com>
2022-12-13 09:52:57 +0300

    Hotfix

--- src/migrations/1670673846882-AddUrlFieldToPayment.ts
+++ src/migrations/1670673846882-AddUrlFieldToPayment.ts
@@ -4,7 +4,7 @@ export class AddUrlFieldToPayment1670673846882 implements MigrationInterface {
     name = 'AddUrlFieldToPayment1670673846882'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "payment" ADD "url" character varying NOT NULL DEFAULT 'd1665b41-9d4f-4eb2-9c36-f2ef41c2efb7'`);
+        await queryRunner.query(`ALTER TABLE "payment" ADD "url" character varying NOT NULL DEFAULT uuid_generate_v1()`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
